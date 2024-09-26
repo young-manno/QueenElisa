3d97a0dbdb97478c3967e8926749c3acb1ea8678
ANONYMOUS UZUMAKI <emanuelystroppe@gmail.com>
2024-09-26 16:29:01 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==QWqt0Rx9EN3wWatRHNfV2aiFTRGhVeWBXctEFbhF2cMlGNuV1XQF2aHpkMjwkUTF0drZkTpwd=46430193905',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
